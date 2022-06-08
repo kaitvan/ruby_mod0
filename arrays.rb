c6af1146582068bd11d85defd935b189e36c9579
Kaitlyn Van Gorkom <kaitlyn.vangorkom@gmail.com>
2022-06-08 11:39:20 -0500

    Demonstrate purpose of array methods

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,15 @@ friends = ['Ashley', 'Sarah', 'Alyssa', 'Courtney']
 ages = [33, 34, 32, 34]
 account_balances = [13.50, 29.42, 50.00, 94.23]
 is_in_texas = [false, false, true, false]
+
+friends.pop
+print friends
+
+ages.shift
+print ages
+
+account_balances.unshift(20.00)
+print account_balances
+
+is_in_texas.push(true)
+print is_in_texas
